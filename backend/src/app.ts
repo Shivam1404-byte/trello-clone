542c96d6d5c37d96d679d315020c99db0d104496
Shivam1404-byte <shivamchn1111@gmail.com>
2026-02-10 21:19:03 +0530

    add: frontend integration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,20 @@ import { authRoute } from "./routes/authRoutes"
 import { boardRoute } from "./routes/boardRoute"
 import { listRoute } from "./routes/listRoute"
 import { cardRoute } from "./routes/cardRoute"
+import cors from "cors"
+import {config} from "dotenv"
+config()
 
 
 export const app = express()
 
 app.use(express.json())
+
+app.use(cors({
+    origin:process.env.URL,
+    methods:["POST","GET","PUT","DELETE","OPTIONS"],
+    allowedHeaders:["Content-Type","Authorization"]
+}))
 
 app.use('/auth',authRoute)
 app.use('/board',boardRoute)
